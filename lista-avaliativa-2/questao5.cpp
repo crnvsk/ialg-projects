8ef6736cae119db827cf5c202d5fe475cf31d21e
João Pedro Alves Carneiro Valadão <107374203+crnvsk@users.noreply.github.com>
2022-08-28 17:28:13 -0300

    adicao de comentarios

--- lista-avaliativa-2/questao5.cpp
+++ lista-avaliativa-2/questao5.cpp
@@ -7,22 +7,27 @@ struct Resultado{
     int *dezenas;
 };
 
-// Criação da função alocação, que recebe o registro resultado e cria um endereço registro. Depois, aloca um novo vetor de tamanho 5(0-4);
+// Criação da função alocação, que recebe o registro resultado e cria um endereço registro.
+// Depois, aloca dinamicamente um novo vetor do tipo inteiro de tamanho 5(0-4).
 void alocacao(Resultado &registro){
     registro.dezenas = new int[5];
 }
 
 int main(){
 
-    // Declaração das variável sorteio e dos iteradores de entrada;
+    // Declaração da variável sorteios e dos iteradores do loop de entrada.
     int sorteios;
     int iterador1 = 0;
     int iterador2 = 0;
     int i;
 
+
+    // Entrada do numero de sorteios realizados.
     cin >> sorteios;
+    // Cria um vetor qdte(quantidade) do tipo Resultado de tamanho sorteios.
     Resultado qtde[sorteios];
 
+    // Loop de alocação.
     for(int k=0; k<sorteios; k++){
         alocacao(qtde[k]);
         for(int g=0; g<6; g++){
@@ -34,9 +39,12 @@ int main(){
                 i = k;
         }
 
+        // Reset do contador.
         iterador1 = 0;
     }
 
+
+    // Loop de saida.
     for(int k=0; k<6; k++){
         cout << qtde[i].dezenas[k] << " ";
     }
