d5dc2f3c1ed2e1a3db750cf80d817f63548bbcab
João Pedro Alves Carneiro Valadão <107374203+crnvsk@users.noreply.github.com>
2022-08-28 17:14:57 -0300

    resolução da questao 1 e 5

--- lista-avaliativa-2/questao1.cpp
+++ lista-avaliativa-2/questao1.cpp
@@ -1,11 +1,42 @@
 #include <iostream>
-#include <ctype.h>
+#include <string.h>
 
 using namespace std;
 
 int main(){
 
-    // TO DO
+    // Declaração da string frase e do char a, usado para comparação dentro do loop while.
+    string frase; 
+    char a = '0';
+
+    // Entrada na cadeia de caracteres.
+    cin >> frase;
+
+    // Declaração do índice.
+    int i=0;
+    while(i < frase.size()){    // Enquanto índice for menor que o tamanho da frase
+        if(a == frase[i]){      // se o char comparador for igual a frase na posição índice
+            frase.erase(frase.begin()+i);   // vai apagar o ultimo char digitado, a partir da posição começo da frase + índice.
+            i--;    // Decremento do índice dentro da condição.
+        } 
+        else{       // Senão, char comparador vai ser igual a frase na posição índice.
+            a = frase[i];
+        }
+
+        i++;    // Incremento do índice fora da condição.
+    }
+    
+    // Saida na entrada.
+    cout << frase;
 
     return 0;
 }
+
+    /*string frase;
+    cin >> frase;
+    int tam = frase.length();
+    char vetFrase[tam];
+
+    strcpy (vetFrase, frase.c_str());
+    for (int i = 0; i < tam; i++)
+        cout << vetFrase[i];*/
--- lista-avaliativa-2/questao5.cpp
+++ lista-avaliativa-2/questao5.cpp
@@ -1,30 +1,45 @@
 #include <iostream>
-#include <stdio.h>
-#include <stdlib.h> 
 
 using namespace std;
 
+// Criação do registro que irá receber o resultado de um sorteio.
 struct Resultado{
-    int vetor[5];
-    int *vet[];
+    int *dezenas;
 };
+
+// Criação da função alocação, que recebe o registro resultado e cria um endereço registro. Depois, aloca um novo vetor de tamanho 5(0-4);
+void alocacao(Resultado &registro){
+    registro.dezenas = new int[5];
+}
 
 int main(){
 
+    // Declaração das variável sorteio e dos iteradores de entrada;
     int sorteios;
+    int iterador1 = 0;
+    int iterador2 = 0;
+    int i;
+
     cin >> sorteios;
-    Resultado *result = new Resultado[4];
+    Resultado qtde[sorteios];
 
-    int *ptrSorteios = new int[5+1];
-    for(int i=0; i<5+1; i++){
-        cin >> ptrSorteios[i];
+    for(int k=0; k<sorteios; k++){
+        alocacao(qtde[k]);
+        for(int g=0; g<6; g++){
+            cin >> qtde[k].dezenas[g];
+            iterador1 += qtde[k].dezenas[g];
+        }
+        if(iterador2 < iterador1){
+                iterador2 = iterador1;
+                i = k;
+        }
+
+        iterador1 = 0;
     }
 
-    for(int i=0; i<5+1; i++){
-        cout << ptrSorteios[i] << " ";
+    for(int k=0; k<6; k++){
+        cout << qtde[i].dezenas[k] << " ";
     }
-
-    delete ptrSorteios;
 
     return 0;
 }
